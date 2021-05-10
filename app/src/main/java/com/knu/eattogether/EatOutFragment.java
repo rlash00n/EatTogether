cba3b7687d5a31c3bf98c96b257044168f098122
rlash <rlash00n@naver.com>
2021-05-11 03:13:38 +0900

    ff

---
+++ app/src/main/java/com/knu/eattogether/EatOutFragment.java
@@ -0,0 +1,19 @@
+package com.knu.eattogether;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EatOutFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_eat_out, container, false);
+    }
+}
--- app/src/main/java/com/knu/eattogether/MainActivity.java
+++ app/src/main/java/com/knu/eattogether/MainActivity.java
@@ -1,14 +1,72 @@
 package com.knu.eattogether;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+    DeliveryFragment deliveryFragment;
+    EatOutFragment eatOutFragment;
+    SettingsFragment settingsFragment;
+
+    TextView main_tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        main_tv = findViewById(R.id.main_text);
+
+        deliveryFragment = new DeliveryFragment();
+        eatOutFragment = new EatOutFragment();
+        settingsFragment = new SettingsFragment();
+
+        getSupportFragmentManager().beginTransaction().
+                replace(R.id.main_frame_layout, deliveryFragment)
+                .commitAllowingStateLoss();
+
+        bottomNavigationView.setSelectedItemId(R.id.tab1);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.tab1:
+                        main_tv.setText("배달 투게더");
+                        getSupportFragmentManager().beginTransaction().
+                                replace(R.id.main_frame_layout, deliveryFragment)
+                                .commitAllowingStateLoss();
+                        return true;
+
+                    case R.id.tab2:
+                        main_tv.setText("외식 투게더");
+                        getSupportFragmentManager().beginTransaction().
+                                replace(R.id.main_frame_layout, eatOutFragment)
+                                .commitAllowingStateLoss();
+                        return true;
+
+                    case R.id.tab3:
+                        main_tv.setText("설정");
+                        getSupportFragmentManager().beginTransaction().
+                                replace(R.id.main_frame_layout, settingsFragment)
+                                .commitAllowingStateLoss();
+                        return true;
+                }
+                return false;
+            };
+        });
     }
+
+
 }
